ddfb7bba767af5e25ab592ccbcc19caaffaec194
Limewood <hem@jordefamn.se>
2013-06-03 00:10:15 +0200

    Removed unused files, added link to repo

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
-WebNewsPaper, (TODO: licensed under the GPL v3), <a href="javascript:show_privacy()">Privacy Policy</a>.
+<a href="https://github.com/Limewood/webnewspaper">WebNewspaper</a>, licensed under the GPL v3, <a href="javascript:show_privacy()">Privacy Policy</a>.
 <div id="privacy"><?php include("privacy_policy.html"); ?></div>
